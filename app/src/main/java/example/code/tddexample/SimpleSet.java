79b34721209e5bf70dd18f11a7303a4f5f7c6e95
Damian <odissey@gmail.com>
2018-08-03 09:49:36 +0100

    Initial commit

---
+++ app/src/main/java/example/code/tddexample/SimpleSet.java
@@ -0,0 +1,39 @@
+package example.code.tddexample;
+
+public class SimpleSet {
+
+    private int[] container = new int[10];
+    private int addedElements = 0;
+
+    public boolean isEmpty() {
+        return addedElements == 0;
+    }
+
+    public boolean addElement(int i) {
+        if (this.contains(i)) return false;
+        container[addedElements++] = i;
+        return true;
+    }
+
+    public int elementCount() {
+        return addedElements;
+    }
+
+    public boolean contains(int i) {
+        for(int element : container) {
+            if(element == i) return true;
+        }
+        return false;
+    }
+
+    public void removeElement(int i) {
+        for (int j = 0; j < container.length; j++) {
+            if (container[j] == i) {
+                for (int k = j; k < container.length - 1; k++) {
+                    container[k] = container[k+1];
+                }
+                addedElements--;
+            }
+        }
+    }
+}
---
+++ app/src/test/java/example/code/tddexample/SimpleSetTest.java
@@ -0,0 +1,81 @@
+package example.code.tddexample;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SimpleSetTest {
+
+    SimpleSet simpleSet;
+    
+    @Before
+    public void setUp() throws Exception {
+        simpleSet = new SimpleSet();
+    }
+
+    @Test
+    public void simpleSet_ifSimpleSetIsEmpty_returnTrue() {
+        assertTrue(simpleSet.isEmpty());
+    }
+
+    @Test
+    public void simpleSet_ifSimpleSetIsNotEmpty_returnFalse() {
+        simpleSet.addElement(6);
+
+        assertFalse(simpleSet.isEmpty());
+    }
+
+    @Test
+    public void simpleSet_ifSimpleSetIsNotEmpty_returnCount() {
+        simpleSet.addElement(1);
+        simpleSet.addElement(2);
+
+        assertEquals(2, simpleSet.elementCount());
+    }
+
+    @Test
+    public void simpleSet_ifSimpleSetContainsElement_returnTrue() {
+        simpleSet.addElement(1);
+        simpleSet.addElement(2);
+
+        assertTrue(simpleSet.contains(1));
+        assertTrue(simpleSet.contains(2));
+    }
+
+    @Test
+    public void simpleSet_ifSimpleSetDoesNotContainElement_returnFalse() {
+        assertFalse(simpleSet.contains(1));
+    }
+
+    @Test
+    public void simpleSet_ifElementRemoved_setDoesNotContainElement() {
+        simpleSet.addElement(1);
+
+        assertTrue(simpleSet.contains(1));
+
+        simpleSet.removeElement(1);
+
+        assertFalse(simpleSet.contains(1));
+    }
+
+    @Test
+    public void simpleSet_ifElementRemoved_returnNewCount() {
+        simpleSet.addElement(1);
+
+        assertTrue(simpleSet.contains(1));
+        assertEquals(1, simpleSet.elementCount());
+
+        simpleSet.removeElement(1);
+
+        assertFalse(simpleSet.contains(1));
+        assertEquals(0, simpleSet.elementCount());
+    }
+
+    @Test
+    public void simpleSet_ifDuplicatedElementAdded_returnFalse() {
+        simpleSet.addElement(1);
+
+        assertFalse(simpleSet.addElement(1));
+    }
+}
